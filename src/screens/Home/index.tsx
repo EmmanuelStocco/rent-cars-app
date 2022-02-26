3899bc89e12cccef8c3df558767f60b5d90ce4e3
EmmanuelStocco <56724388+EmmanuelStocco@users.noreply.github.com>
2022-02-26 14:07:06 -0300

    first - project created and configs init

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+                          
+import { StyleSheet, Text, View } from 'react-native';
+                          
+import {
+    Container,
+    Title
+} from './styles';
+                          
+export function Home(){
+                          
+   return (
+     
+           <Container>
+               <Title>
+                   Hellow World
+               </Title>
+           </Container>
+  
+      );
+     
+    }
