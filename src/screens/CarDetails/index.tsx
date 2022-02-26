447b07331acccf9fdc907a0261fadaaf07d22f22
EmmanuelStocco <56724388+EmmanuelStocco@users.noreply.github.com>
2022-02-26 20:17:08 -0300

    feat: car details screen and created

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-                          
-import { StyleSheet, Text, View } from 'react-native';
-import { BackButton } from '../../components/BackButton';
+                           
+import { BackButton } from '../../components/BackButton'; 
 import { ImageSlider } from '../../components/ImageSlider';
 
                           
@@ -9,6 +8,16 @@ import {
     Container,
     Header,
     CarImages,
+    Content,
+    Details,
+    Description,
+    Brand,
+    Name,
+    Rent,
+    Period,
+    Price,
+    About,
+
 } from './styles';
                           
 export function CarDetails(){
@@ -21,7 +30,28 @@ export function CarDetails(){
 
             <CarImages>
                 <ImageSlider  imagesUrl={['https://www.pngplay.com/wp-content/uploads/13/Audi-RS5-PNG-Free-File-Download.png']} />
-            </CarImages>             
+            </CarImages>       
+
+            <Content>
+                <Details>
+                    <Description>
+                        <Brand>Lamborghini</Brand>
+                        <Name>Huracan</Name>
+                    </Description>
+
+                   <Rent>
+                        <Period>Ao dia</Period>
+                        <Price>R$ 580</Price>
+                    </Rent> 
+                </Details>
+
+                <About>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                  Sit voluptatum vel, saepe, temporibus voluptatem aperiam 
+                  earum laudantium est iste commodi cum nam veritatis quia 
+                </About>
+            </Content>
+
            </Container>
   
       ); 
