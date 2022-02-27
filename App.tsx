5eb0599cc5a056d576a324b465c2e651dcb5dd9f
EmmanuelStocco <56724388+EmmanuelStocco@users.noreply.github.com>
2022-02-27 01:24:15 -0300

    feat: schedulling details following ux patterns

--- App.tsx
+++ App.tsx
@@ -10,7 +10,7 @@ import {
   Archivo_600SemiBold
 } from '@expo-google-fonts/archivo'
 import { ThemeProvider } from 'styled-components'
-import { Scheduling } from './src/screens/Scheduling'; 
+import { SchedullingDetails } from './src/screens/SchedullingDetails'; 
 import AppLoading from 'expo-app-loading';
 import theme from './src/styles/theme';
 
@@ -30,6 +30,6 @@ export default function App(){
  
   return (
     <ThemeProvider theme={theme}>
-      <Scheduling /> 
+      <SchedullingDetails /> 
     </ThemeProvider>
   )}
---
+++ src/screens/SchedullingDetails/index.tsx
@@ -0,0 +1,123 @@
+import React from 'react';
+import {Feather} from '@expo/vector-icons'
+                           
+import { BackButton } from '../../components/BackButton'; 
+import { ImageSlider } from '../../components/ImageSlider';
+import { Acessory } from '../../components/Acessory';
+import { Button } from '../../components/Button';
+
+import speedSvg from '../../assets/speed.svg'
+import acellerationSvg from '../../assets/acceleration.svg'
+import forceSvg from '../../assets/force.svg'
+import gasolineSvg from '../../assets/gasoline.svg'
+import exchangeSvg from '../../assets/exchange.svg'
+import peopleSvg from '../../assets/people.svg'
+
+                          
+import {
+    Container,
+    Header,
+    CarImages,
+    Content,
+    Details,
+    Description,
+    Brand,
+    Name,
+    Rent,
+    Period,
+    Price, 
+    Acessories,
+    Footer,
+    RentalPeriod,
+    CalendarIcon,
+    DateInfo,
+    DateTitle,
+    DateValue, 
+    RentalPrice,
+    RentalPriceLabel,
+    RentalPriceDetails,
+    RentalPriceQuota,
+    RentalPriceTotal,
+ 
+} from './styles'; 
+import { RFValue } from 'react-native-responsive-fontsize';
+import theme from '../../styles/theme';
+import { useTheme } from 'styled-components';
+                          
+export function SchedullingDetails (){
+   const theme = useTheme();                        
+   return ( 
+           <Container>
+                <Header>
+                    <BackButton onPress={() => {}} /> 
+                </Header>   
+
+            <CarImages>
+                <ImageSlider  imagesUrl={['https://www.pngplay.com/wp-content/uploads/13/Audi-RS5-PNG-Free-File-Download.png']} />
+            </CarImages>       
+
+            <Content>
+                <Details>
+                    <Description>
+                        <Brand>Lamborghini</Brand>
+                        <Name>Huracan</Name>
+                    </Description>
+
+                   <Rent>
+                        <Period>Ao dia</Period>
+                        <Price>R$ 580</Price>
+                    </Rent> 
+                </Details>
+
+                <Acessories>
+                    <Acessory name="380Km/h" icon={speedSvg}/>
+                    <Acessory name="3.2s" icon={acellerationSvg}/>
+                    <Acessory name="800 HP" icon={forceSvg}/>
+                    <Acessory name="Gasolina" icon={gasolineSvg}/>
+                    <Acessory name="Auto" icon={exchangeSvg}/>
+                    <Acessory name="2 pessoas" icon={peopleSvg}/>
+                </Acessories>
+
+                <RentalPeriod>
+                    <CalendarIcon> 
+                        <Feather 
+                            name='calendar'
+                            size={RFValue(24)}
+                            color={theme.colors.shape}
+                        />
+                    </CalendarIcon>
+             
+                    <DateInfo>
+                        <DateTitle> DE </DateTitle>
+                        <DateValue> 18/06/2021 </DateValue>
+                    </DateInfo>
+
+                    <Feather
+                        name='chevron-right'
+                        size={RFValue(24)}
+                        color={theme.colors.text}
+                    />
+
+                    <DateInfo>
+                        <DateTitle> DE </DateTitle>
+                        <DateValue> 18/06/2021 </DateValue>
+                    </DateInfo> 
+                </RentalPeriod> 
+
+                <RentalPrice>
+                    <RentalPriceLabel> TOTAL </RentalPriceLabel>
+                    <RentalPriceDetails>
+                        <RentalPriceQuota> R$ 580 x3 diárias </RentalPriceQuota>
+                        <RentalPriceTotal> R$ 2.900 </RentalPriceTotal>
+                    </RentalPriceDetails>
+                </RentalPrice>
+            </Content> 
+
+            <Footer>
+                <Button title='Confirmar' color=''/>
+            </Footer>
+
+           </Container>
+  
+      ); 
+    }
